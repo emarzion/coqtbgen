651f04bab1b415eee83f299d68ffab6457218b9e
emarzion <emarzion@gmail.com>
2023-09-13 16:06:06 -0500

    some playing functionality

--- bin/web/play.ml
+++ bin/web/play.ml
@@ -7,12 +7,19 @@ open BearGame
 open ExtractQuery
 open Read_file
 open Draw
+open Strategy
 
 module Model = struct
 
-  type t = coq_BG_State
+  type t = 
+    { curr_state : coq_BG_State;
+      curr_tb_strategy : strategy;
+    }
 
-  let init = init_RW_State
+  let init tb =
+    { curr_state = init_RW_State;
+      curr_tb_strategy = rw_tb_strat Player.White (Obj.magic init_RW_State) tb;
+    }
 
 end
 
@@ -23,18 +30,34 @@ type rp = rs * rf
 module Action = struct
   type t =
     | ClickMove of coq_BG_Move
+    | ClickStep
 end
 
 module Controller = struct
   open Action
 
   let update (a : Action.t) ((rs,rf) : rp) =
-    let curr = React.S.value rs in
-    let m =
+    let Model.{ curr_state; curr_tb_strategy }  = React.S.value rs in
+    let t =
       match a with
-      | ClickMove m -> exec_move RomanWheel.coq_RomanWheel curr m
+      | ClickMove m ->
+        let strat = Lazy.force curr_tb_strategy in (
+          match strat with
+          | Coq_abelard_strategy(_,strats) ->
+            let s = exec_move RomanWheel.coq_RomanWheel curr_state m in
+            let str' = strats (Obj.magic m) in Model.{ curr_state = s; curr_tb_strategy = str' }
+          | _ -> failwith "mismatch."
+          )
+      | ClickStep ->
+        let strat = Lazy.force curr_tb_strategy in (
+          match strat with
+          | Coq_eloise_strategy(_,m,str) ->
+            let s = exec_move RomanWheel.coq_RomanWheel curr_state (Obj.magic m) in
+            Model.{ curr_state = s; curr_tb_strategy = str }
+          | _ -> failwith "mismatch."
+        )
     in
-    rf m
+    rf t
 end
 
 module View = struct
@@ -45,13 +68,15 @@ module View = struct
     )
 
   let pure_bear x =
-    let pos = coords (Obj.magic (x.BearGame.bear)) in
+    let Model.{ curr_state; _ } = x in
+    let pos = coords (Obj.magic (curr_state.BearGame.bear)) in
     [Js_of_ocaml_tyxml.Tyxml_js.Svg.(
       circle ~a:[a_stroke (`Color ("black", None)); a_fill (`Color ("black", None)); a_cx (fst pos, Some `Px); (a_cy (snd pos, Some `Px)); (a_r (7.5, Some `Px))] []
     )]
 
   let pure_hunters x =
-    let hs = List.map (fun x -> coords (Obj.magic x)) x.BearGame.hunters in
+    let Model.{ curr_state; _ } = x in
+    let hs = List.map (fun x -> coords (Obj.magic x)) curr_state.BearGame.hunters in
     List.map (fun pos ->
       Js_of_ocaml_tyxml.Tyxml_js.Svg.(
         circle ~a:[a_stroke (`Color ("black", None)); a_fill (`Color ("white", None)); a_cx (fst pos, Some `Px); (a_cy (snd pos, Some `Px)); (a_r (7.5, Some `Px))] []
@@ -65,9 +90,10 @@ module View = struct
       svg [Js_of_ocaml_tyxml.Tyxml_js.R.Svg.g pieces]
     )
 
-  let pure_curr_res mp s =
+  let pure_curr_res mp x =
+    let Model.{ curr_state; _ } = x in
     let text = (
-      match ExtractQuery.query_RW_TB mp s with
+      match ExtractQuery.query_RW_TB mp curr_state with
       | Some (pl, n) -> (
         match pl with
         | Player.White -> "White wins in " ^ string_of_int n
@@ -81,42 +107,33 @@ module View = struct
     let sig_curr = ReactiveData.RList.from_signal (React.S.map (pure_curr_res mp) rs) in
     Js_of_ocaml_tyxml.Tyxml_js.R.Html.p sig_curr
 
-  let pure_move_links mp (rs,rf) s =
-    let moves = enum_moves RomanWheel.coq_RomanWheel s in
-    let move_tups = List.map (fun m ->
-      let s' = coq_RW_exec_move s m in
-      (m, ExtractQuery.query_RW_TB mp s')
-    ) moves in
-    let move_tups_sorted = List.sort (fun (_,o1) (_,o2) ->
-      if OCamlTB.p_leb s.to_play o1 o2 then 1 else -1
-      ) move_tups in
-    let onclick m _ =
+  let pure_move_links (rs,rf) x =
+    let Model.{ curr_state; curr_tb_strategy } = x in
+    let moves = enum_moves RomanWheel.coq_RomanWheel curr_state in
+    let clickmove m _ =
       Controller.update (ClickMove m) (rs, rf); true in
-    List.concat_map (fun (m,o) ->
-      let str = (
-        match o with
-        | Some (pl, n) -> (
-          match pl with
-          | Player.White -> "White wins in " ^ string_of_int n
-          | Player.Black -> "Black wins in " ^ string_of_int n
-          )
-        | None -> "Drawn"
-        ) in
-      let text = print_RW_move s m ^ ": " ^ str in
-      [Tyxml_js.Html.(a ~a:[a_href "#"; a_onclick (onclick m); a_class ["whitewin"]; a_style "text-decoration: none"] [txt text]);
-       Tyxml_js.Html.br ()
-      ]
-      ) move_tups_sorted
+    let clickstep _ =
+      Controller.update ClickStep (rs, rf); true in
+    let strat = Lazy.force curr_tb_strategy in
+    match strat with
+    | Coq_eloise_strategy(_,_,_) -> [Tyxml_js.Html.(a ~a:[a_href "#"; a_onclick clickstep; a_class ["whitewin"]; a_style "text-decoration: none"] [txt "step"])]
+    | _ ->
+      List.concat_map (fun m ->
+        let text = print_RW_move curr_state m in
+        [Tyxml_js.Html.(a ~a:[a_href "#"; a_onclick (clickmove m); a_class ["whitewin"]; a_style "text-decoration: none"] [txt text]);
+        Tyxml_js.Html.br ()
+        ]
+        ) moves
 
-  let links mp (rs, rf) =
-    let vals = ReactiveData.RList.from_signal (React.S.map (pure_move_links mp (rs, rf)) rs) in
+  let links (rs, rf) =
+    let vals = ReactiveData.RList.from_signal (React.S.map (pure_move_links (rs, rf)) rs) in
     Js_of_ocaml_tyxml.Tyxml_js.R.Html.p vals
 
   let board (rs,rf) =
     Tyxml_js.Html.svg (circ :: lines @ arcs @ [pieces (rs,rf)])
 
   let tablebase mp (rs,rf) =
-    Tyxml_js.Html.(div ~a:[a_class ["tablebase"]] [curr_res mp (rs, rf); board (rs,rf); links mp (rs,rf)])
+    Tyxml_js.Html.(div ~a:[a_class ["tablebase"]] [curr_res mp (rs, rf); board (rs,rf); links (rs,rf)])
 
   let draw_stuff mp rp node =
     Dom.appendChild node (Tyxml_js.To_dom.of_node (tablebase mp rp));
@@ -133,7 +150,7 @@ let main mp _ =
     Js.Opt.get (doc##getElementById(Js.string "beargame"))
       (fun () -> assert false)
     in
-  let rp = React.S.create Model.init in
+  let rp = React.S.create (Model.init mp) in
     start mp rp parent
 
 let _ =
